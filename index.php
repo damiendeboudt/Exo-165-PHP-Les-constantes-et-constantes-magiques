5be7928384f175c6cea720ce30721c57a153c1b2
Damien <damien.deboudt@sfr.fr>
2023-01-18 09:43:20 +0100

    ok

--- index.php
+++ index.php
@@ -9,9 +9,26 @@
  *    longueur de la chaîne de la troisième constante ( attention aux priorités )
  */
 // TODO Votre code ici.
+const MACONST1 = 12;
+const MACONST2 = 2;
+echo "Resultat de constante 1 * constante 2 = ".MACONST1 * MACONST2 . "<br>";
 
+
+const MACONST3 = "C'est cool PHP";
+echo "Longueur de constante3 = ".strlen(MACONST3). "<br>";
+
+$result = (MACONST1 + MACONST2) * strlen(MACONST3);
+echo "Resultat de (const1 + const2) * longueur const3 = ".$result . "<br>";
+
+
+echo "<br><br>";
+echo "Info sur le fichier: <br>";
+echo "- ".basename(__DIR__). "<br>";
+echo "- ".basename(__FILE__). "<br>";
+echo "- ".maClasse::class . "<br>";
 
 /**
- * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
+ * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum
+ * d'informations sur la page actuelle4
  */
 // TODO Votre code ici.
